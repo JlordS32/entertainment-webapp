29f520a7992c04e1b328f8972cef8fe316c36ac4
JlordS32 <jlordsty128@gmail.com>
2023-12-05 22:19:07 +1100

    setting styles

---
+++ entertain-app/src/app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+	return (
+		<main>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque odio
+					dolore ullam quae suscipit doloribus mollitia voluptatem dolor
+					ducimus, at voluptatibus! Aliquam pariatur fuga ut mollitia quaerat
+					provident inventore quas.
+				</p>
+			</div>
+		</main>
+	);
+}
