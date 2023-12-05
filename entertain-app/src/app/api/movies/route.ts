d90986b000cb00ca6f32781917614cc9781624d4
JlordS32 <jlordsty128@gmail.com>
2023-12-05 21:59:07 +1100

    added GET api req with pagination for infinite scrolling

--- entertain-app/src/app/api/movies/route.ts
+++ entertain-app/src/app/api/movies/route.ts
@@ -1,10 +1,29 @@
-import { NextResponse } from 'next/server';
+// next imports
+import { NextApiRequest } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+
+// data
 import data from '../../../data/data.json';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
-	return NextResponse.json({
-		title: 'test',
-	});
+const queries = ['limit', 'page'];
+
+export async function GET(req: NextRequest) {
+	const searchParams = req.nextUrl.searchParams;
+	const query = queries.reduce((acc: { [key: string]: string }, item) => {
+		const queryParams = searchParams.get(item);
+		if (queryParams) {
+			acc[item] = queryParams;
+		}
+		return acc;
+	}, {});
+
+	if (query && query.limit || query.page) {
+		const startIndex = (Number(query.page) - 1) * Number(query.limit) || 0;
+		const endIndex = startIndex + Number(query.limit);
+		return NextResponse.json(data.slice(startIndex, endIndex));
+	}
+
+	return NextResponse.json(data);
 }
