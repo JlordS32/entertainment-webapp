9c18ea09796d5847175f3264a420dab1ecc52681
JlordS32 <jlordsty128@gmail.com>
2023-12-05 22:00:45 +1100

    removed import

--- entertain-app/src/app/api/movies/route.ts
+++ entertain-app/src/app/api/movies/route.ts
@@ -1,5 +1,4 @@
 // next imports
-import { NextApiRequest } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
 // data
@@ -7,23 +6,31 @@ import data from '../../../data/data.json';
 
 export const dynamic = 'force-dynamic';
 
+// Change as needed for query handling.
 const queries = ['limit', 'page'];
 
+/**
+ * Handles the GET request
+ * @param req The NextRequest object
+ */
 export async function GET(req: NextRequest) {
-	const searchParams = req.nextUrl.searchParams;
-	const query = queries.reduce((acc: { [key: string]: string }, item) => {
-		const queryParams = searchParams.get(item);
-		if (queryParams) {
-			acc[item] = queryParams;
-		}
-		return acc;
-	}, {});
+   // Extract the query parameters from the URL
+   const searchParams = req.nextUrl.searchParams;
+   const query = queries.reduce((acc: { [key: string]: string }, item) => {
+      const queryParams = searchParams.get(item);
+      if (queryParams) {
+         acc[item] = queryParams;
+      }
+      return acc;
+   }, {});
 
-	if (query && query.limit || query.page) {
-		const startIndex = (Number(query.page) - 1) * Number(query.limit) || 0;
-		const endIndex = startIndex + Number(query.limit);
-		return NextResponse.json(data.slice(startIndex, endIndex));
-	}
+   // Check if limit or page query parameters are present
+   if (query && (query.limit || query.page)) {
+      // Calculate the start and end indices for slicing the data
+      const startIndex = (Number(query.page) - 1) * Number(query.limit) || 0;
+      const endIndex = startIndex + Number(query.limit);
+      return NextResponse.json(data.slice(startIndex, endIndex));
+   }
 
-	return NextResponse.json(data);
+   return NextResponse.json(data);
 }
